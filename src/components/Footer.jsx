ba22d0d5d63f6868c7ab16c027db27695e07eefe
Siddharth Khanna <siddharthkhanna2k@gmail.com>
2023-04-28 19:43:57 -0400

    update security flaws in links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,15 @@ export const Footer = () => {
             <section id="contact">
                 <footer className="footer">
                     <div className="social-icons">
-                        <a href="mailto:siddharthkhanna2k@gmail.com" target="_blank">
+                        <a href="mailto:siddharthkhanna2k@gmail.com" target="_blank" rel="noreferrer" >
                             <i class="fab fa-envelope"></i>
                         </a>
 
-                        <a href="https://www.linkedin.com/in/siddharth-khanna-31a90215a/" target="_blank">
+                        <a href="https://www.linkedin.com/in/siddharth-khanna-31a90215a/" target="_blank" rel="noreferrer" >
                             <i class="fab fa-linkedin"></i>
                         </a>
 
-                        <a href="https://github.com/siddkhannaa" target="_blank">
+                        <a href="https://github.com/siddkhannaa" target="_blank" rel="noreferrer" >
                             <i class="fab fa-github"></i>
                         </a>
                     </div>
